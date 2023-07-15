a9229e31205497409af021b7f0ad7b0bbb8f2aed
Ain1204 <122449772+Ain1204@users.noreply.github.com>
2023-07-15 23:38:05 +0900

    Add files via upload

---
+++ debateMain.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.whiteBtn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+    button.classList.add('active');
+  });
+});
