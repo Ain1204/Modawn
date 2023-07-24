ba1c188d3e45870cbfb76701e0be95e28287f141
Ain1204 <emily030528@gmail.com>
2023-07-24 17:18:54 +0900

    ain edit
    login: post>get

--- 아인-수정함/Login.js
+++ 아인-수정함/Login.js
@@ -1,15 +1,7 @@
 "use strict";
 
-fetch("http://43.200.164.174:3000/api/user/login", {
-    method: "POST",
-    body: JSON.stringify({
-      email: id,
-      password: pw,
-    }),
-  })
-  .then((response) => response.json())
-  .then((response)=>{
-    console.log(response.data)
- 	  localStorage.setItem('token', response.token);
-  })
-;
+
+fetch("http://43.200.164.174:3000/api/user/login")
+.then((response)=>response.json())
+.then((response)=>{
+      this.setState({users:response})})
