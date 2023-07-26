a446d357a7b37b057926ea4bf09adbc9a54a296e
Ain1204 <emily030528@gmail.com>
2023-07-26 13:18:06 +0900

    update

--- 아인-수정함/Login.js
+++ 아인-수정함/Login.js
@@ -16,11 +16,10 @@ fetch('http://43.200.164.174:3000/api/user/login', {
           password : password,
       })
   })
-  .then(res => res.json())
-  .then(res => {
-      if(res.token){
-          localStorage.setItem("token", res.token)
-      }else{
-          window.alert(res.msg)
-      }
+  .then(response => response.json())
+  .then(response => {
+      if(response.token){
+          localStorage.setItem("token", response.token);
+      }else alert('아이디 / 비밀번호를 다시 입력해주세요!');
+      
 });
