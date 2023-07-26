b0916083b3517c0c387aefeac4560cabcc86bbd8
Ain1204 <emily030528@gmail.com>
2023-07-26 14:03:43 +0900

    멤버십 수정해봄..!

--- 아인-수정함/Login.js
+++ 아인-수정함/Login.js
@@ -5,21 +5,20 @@ fetch("http://43.200.164.174:3000/api/user/login")
 .then((data) => console.log(data));
 */
 
+
 fetch('http://43.200.164.174:3000/api/user/login', {
-      method : "POST",
-      headers : {
-          'Content-Type' : 'application/json',
-          'Accept' : 'application/json',
-      },
-      body : JSON.stringify({
-            email : id,
-          password : password,
-      })
-  })
-  .then(response => response.json())
-  .then(response => {
-      if(response.token){
-          localStorage.setItem("token", response.token);
-      }else alert('아이디 / 비밀번호를 다시 입력해주세요!');
-      
+  method: "GET", // GET 요청으로 변경
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  // GET 요청의 경우에는 body를 사용하지 않습니다.
+})
+.then(response => response.json())
+.then(response => {
+  if (response.token) {
+    localStorage.setItem("token", response.token);
+  } else {
+    alert('아이디 / 비밀번호를 다시 입력해주세요!');
+  }
 });
--- 아인-수정함/membership.js
+++ 아인-수정함/membership.js
@@ -8,20 +8,41 @@ function submit(event) {
 
 form.addEventListener("click", submit);
 */
+function registerUser() {
+    const username = document.getElementById('username').value;
+    const nickname = document.getElementById('nickname').value;
+    const id = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordCheck = document.getElementById('confirmPassword').value;
 
-fetch('http://43.200.164.174:3000/api/user/register',{
-	method : "post",
-    headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-   	body : JSON.stringify({
-            name : username,
-            nickname : nickname,
-            email : id,
-            password: password,
-            confirmPassword : passwordCheck,
-        }),
-})
-.then((response) => response.json())
-.then((response) => console.log(response));
+    // 회원가입 정보를 객체로 만들기
+    const userData = {
+        name: username,
+        nickname: nickname,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+    };
+
+
+    fetch("http://43.200.164.174:3000/api/user/register",{
+        method : "post",
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(userData), // 객체 데이터를 문자열로 변환하여 전달
+    })  
+    .then((response) => {
+        if (response.success) {
+            // 회원가입 성공 시 Login.html로 이동
+            window.location.href = "Login.html";
+        } else {
+            // 회원가입 실패 시 에러 메시지 출력 또는 다른 처리
+            console.log("회원가입에 실패하였습니다.");
+        }
+    })
+    .catch((error) => {
+        console.error("오류 발생:", error);
+    });
+}
