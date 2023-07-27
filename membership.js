43dc6d984eddcb90e070830c56e27fe198b322dd
yellowmodric <87848941+yellowmodric@users.noreply.github.com>
2023-07-27 23:22:37 +0900

    membership.js

--- membership.js
+++ membership.js
@@ -19,41 +19,6 @@ function input() {
 
 function submit(event) {
     event.preventDefault();
-
-    const url = "http://43.200.164.174:3000/api/user/register";
-    const username = usernameInput.value;
-    const nickname = nicknameInput.value;
-    const id = emailInput.value;
-    const password = passwordInput.value;
-    const passwordCheck = passwordCheckInput.value;
-    
-    fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            name: username,
-            nickname: nickname,
-            email: id,
-            password: password,
-            confirmPassword: passwordCheck
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(res => res.json())
-    .then(data => {
-        if (data) {
-            console.log(data);
-            alert("성공!");
-        } else {
-            throw new Error("서버 응답에 오류가 있습니다.");
-        }
-    })
-    .catch(err => {
-        console.error(err);
-        alert("서버 응답 처리에 실패했습니다.");
-    });
-
 }
 
 membershipJoin.addEventListener("submit", submit);
