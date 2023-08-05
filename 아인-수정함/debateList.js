8c1fa7749693ddd8eaacfd4877a0ca99a50fdb38
jiho5993 <jiho5993@naver.com>
2023-08-06 00:48:35 +0900

    feat: 토론 상세페이지에서 의견 보여주는 로직 구현

--- 아인-수정함/debateList.js
+++ 아인-수정함/debateList.js
@@ -55,47 +55,81 @@ async function getDiscussionInfo(discussionIdx) {
     }
 
     const debateInfo = responseData.data;
-    const titleTitleElem = document.querySelector(".titleTitle");
-    if (debateInfo.discussion.user.idx === userInfo.idx) {
-      titleTitleElem.innerHTML += `
-        <button class="whiteBtn"><a href="mainsubject.html">수정</a></button>
-        <button class="whiteBtn">삭제</button>
-      `;
-    } else {
-      titleTitleElem.innerHTML += `
-        <button class="redBtn">신고</button>
-      `;
-    }
 
-    // 서버에서 가져온 데이터로 토론 정보를 채워줍니다.
-    const imgUrlContentElement = document.querySelector(".imgUrl");
-    const urlElement = document.querySelector(".url a");
-    const titleElement = document.querySelector(".titleTitle .title1");
-    const statusElement = document.querySelector(".titleTitle .ongoing");
-    const nicknameElement = document.querySelector("div.titleName span.nickname");
-    const contentElement = document.querySelector(".content");
-    const debateCategoryElement = document.querySelector(".debateCategory");
+    fillDebateInfo(debateInfo, userInfo);
+    fillOpinionInfo(debateInfo.discussion.opinionApproves, debateInfo.discussion.opinionDisapproves, debateInfo.discussion.opinionOthers);
 
-    if (debateInfo.discussion.imgUrl) {
-      imgUrlContentElement.src = debateInfo.discussion.imgUrl;
-    } else {
-      imgUrlContentElement.remove();
-    }
-
-    urlElement.textContent = debateInfo.discussion.url;
-    urlElement.href = debateInfo.discussion.url;
-    titleElement.textContent = debateInfo.discussion.title;
-    statusElement.textContent = debateInfo.discussion.status === 'DISCUSS' ? '진행중' : '마감';
-    nicknameElement.textContent = debateInfo.discussion.user.nickname;
-    contentElement.textContent = debateInfo.discussion.content;
-    debateCategoryElement.textContent = debateInfo.discussion.discussion_category.type;
-
-    // endDate 값을 "토론 마감: " 뒤에 보이도록 요소를 업데이트합니다.
-    const endDateElement = document.querySelector(".titleName span:last-child");
-    endDateElement.textContent = `토론 마감: ${debateInfo.discussion.endDate}`;
   } catch (error) {
     console.error("토론 정보를 가져오는 중 오류가 발생했습니다:", error);
   }
+}
+
+function fillDebateInfo(debateInfo, userInfo) {
+  const titleTitleElem = document.querySelector(".titleTitle");
+  if (debateInfo.discussion.user.idx === userInfo.idx) {
+    titleTitleElem.innerHTML += `
+        <button class="whiteBtn"><a href="mainsubject.html">수정</a></button>
+        <button class="whiteBtn">삭제</button>
+      `;
+  } else {
+    titleTitleElem.innerHTML += `
+        <button class="redBtn">신고</button>
+      `;
+  }
+
+  // 서버에서 가져온 데이터로 토론 정보를 채워줍니다.
+  const imgUrlContentElement = document.querySelector(".imgUrl");
+  const urlElement = document.querySelector(".url a");
+  const titleElement = document.querySelector(".titleTitle .title1");
+  const statusElement = document.querySelector(".titleTitle .ongoing");
+  const nicknameElement = document.querySelector("div.titleName span.nickname");
+  const contentElement = document.querySelector(".content");
+  const debateCategoryElement = document.querySelector(".debateCategory");
+
+  if (debateInfo.discussion.imgUrl) {
+    imgUrlContentElement.src = debateInfo.discussion.imgUrl;
+  } else {
+    imgUrlContentElement.remove();
+  }
+
+  urlElement.textContent = debateInfo.discussion.url;
+  urlElement.href = debateInfo.discussion.url;
+  titleElement.textContent = debateInfo.discussion.title;
+  statusElement.textContent = debateInfo.discussion.status === 'DISCUSS' ? '진행중' : '마감';
+  nicknameElement.textContent = debateInfo.discussion.user.nickname;
+  contentElement.textContent = debateInfo.discussion.content;
+  debateCategoryElement.textContent = debateInfo.discussion.discussion_category.type;
+
+  // endDate 값을 "토론 마감: " 뒤에 보이도록 요소를 업데이트합니다.
+  const endDateElement = document.querySelector(".titleName span:last-child");
+  endDateElement.textContent = `토론 마감: ${debateInfo.discussion.endDate}`;
+
+  const sympathyCountElement = document.querySelector("div.sympathyBtn > div > p");
+  sympathyCountElement.textContent = debateInfo.discussionLikeCount;
+}
+
+function fillOpinionInfo(approves, disapproves, others) {
+  const approvesDetailElem = document.querySelector("div.opinion.approve > div.op > span.opinionDetail");
+  const approvesSympathyElem = document.querySelector("div.opinion.approve > div.op > span.sympathy");
+  const disapprovesDetailElem = document.querySelector("div.opinion.disapprove > div.op > span.opinionDetail");
+  const disapprovesSympathyElem = document.querySelector("div.opinion.disapprove > div.op > span.sympathy");
+  const othersDetailElem = document.querySelector("div.opinion.other > div.op > span.opinionDetail");
+  const othersSympathyElem = document.querySelector("div.opinion.other > div.op > span.sympathy");
+
+  approves.forEach((item) => {
+    approvesDetailElem.innerHTML += `<p>${item.title}</p>`;
+    approvesSympathyElem.innerHTML += `<p>좋아요 ${item.likeCount}</p>`;
+  });
+
+  disapproves.forEach((item) => {
+    disapprovesDetailElem.innerHTML += `<p>${item.title}</p>`;
+    disapprovesSympathyElem.innerHTML += `<p>좋아요 ${item.likeCount}</p>`;
+  });
+
+  others.forEach((item) => {
+    othersDetailElem.innerHTML += `<p>${item.title}</p>`;
+    othersSympathyElem.innerHTML += `<p>좋아요 ${item.likeCount}</p>`;
+  });
 }
 
 (() => {
